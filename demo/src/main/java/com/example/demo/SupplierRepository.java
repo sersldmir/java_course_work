91c1d77799a70e7330baf06135e20545e9bb5eea
sersldmir <2sergmir2@gmail.com>
2023-04-28 01:03:03 +0300

    hid id from users fixed not required dropbox issue added validation for phones and emails
    began working on validating if a string is not just spaces

--- demo/src/main/java/com/example/demo/SupplierRepository.java
+++ demo/src/main/java/com/example/demo/SupplierRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long>{
-    @Query("SELECT s from Supplier s WHERE CONCAT(s.supid, ' ', s.name, ' ', s.phone, ' ', s.email) LIKE %?1%")
+    @Query("SELECT s from Supplier s WHERE CONCAT(s.name, ' ', s.phone, ' ', s.email) LIKE %?1%")
     List<Supplier> search(String keyword);
 
     @Query("SELECT s from Supplier s WHERE s.supid LIKE ?1")
